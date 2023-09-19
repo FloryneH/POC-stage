975c20165df568326b80908b970d654572fa5659
Wika <127410155+FloryneH@users.noreply.github.com>
2023-09-19 14:23:53 +0200

    feat/dev/03 : Ajout des infos dans la home (sans les liens)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,20 @@
-import '../css/app.scss'
+import '../css/app.scss';
+import { Dropdown } from 'bootstrap';
+
+document.addEventListener('DOMContentLoaded', () => {
+    new App();
+});
+
+class App {
+    constructor() {
+        this.enableDropdowns();
+        this.handleCommentForm();
+    }
+
+    enableDropdowns() {
+        const dropDownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+        dropDownElementList.map(function (dropdownToggleEl) {
+            return new Dropdown(dropdownToggleEl)
+        });
+    }
+}
