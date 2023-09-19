2dc4548001d9ce537c97b9693f01e372119edeb4
Wika <127410155+FloryneH@users.noreply.github.com>
2023-09-19 16:32:44 +0200

    feat/dev/05 : Commencement de la partie article du dashboard

--- src/Entity/Commentaire.php
+++ src/Entity/Commentaire.php
@@ -7,7 +7,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CommentaireRepository::class)]
-class Commentaire
+class Commentaire 
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
