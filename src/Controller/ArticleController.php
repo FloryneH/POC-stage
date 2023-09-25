f702d0ef10fee4084936149c590d0925da0f1d73
Wika <127410155+FloryneH@users.noreply.github.com>
2023-09-25 16:30:06 +0200

    feat/dev/14 : Gestion des images

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,12 +5,31 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Entity\Comment;
 use App\Form\Type\CommentType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/articles', name: 'article_all')]
+    public function list(): Response
+    {
+        $articleRepository = $this->entityManager->getRepository(Article::class);
+        $articles = $articleRepository->findAll();
+
+        return $this->render('article/all.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+
     #[Route('/article/{slug}', name: 'article_show')]
     public function show(?Article $article): Response
     {
