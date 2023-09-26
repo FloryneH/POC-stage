5553c96957b93c24ebb75f9f4e111eb3a5d648c6
Wika <127410155+FloryneH@users.noreply.github.com>
2023-09-26 09:42:20 +0200

    feat/dev/16 : Affichage de tout les articles et de toutes les formations (en groupe et par page attitrée)

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Formation;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FormationController extends AbstractController
 {
-    #[Route('/formation/{slug}', name: 'formation_show')]
-    public function index(): Response
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        return $this->render('formation/index.html.twig', [
-            'controller_name' => 'FormationController',
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/formations', name: 'formation_all')]
+    public function list(): Response
+    {
+        $formationRepository = $this->entityManager->getRepository(Formation::class);
+        $formations = $formationRepository->findAll();
+
+        return $this->render('formation/all.html.twig', [
+            'formations' => $formations,
+        ]);
+    }
+
+    #[Route('/formation/{slug}', name: 'formation_show')]
+    public function show(?Formation $formation): Response
+    {
+        if (!$formation) {
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('formation/show.html.twig', [
+            'formation'=>$formation,
         ]);
     }
 }
